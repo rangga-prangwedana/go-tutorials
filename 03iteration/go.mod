50c8c841d903fc07e9f4a35a7f57467b5bf7a7bc
rangga-prangwedana <vergil1st@gmail.com>
2026-02-14 20:48:53 +0700

    feat: Added iteration exercise

---
+++ 03iteration/go.mod
@@ -0,0 +1,3 @@
+module rangga.prangwedana.com/iteration
+
+go 1.25.7
---
+++ 03iteration/repeat_test.go
@@ -0,0 +1,27 @@
+package iteration
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestRepeat(t *testing.T) {
+	repeated := Repeat("a", 7)
+	expected := "aaaaaaa"
+
+	if repeated != expected {
+		t.Errorf("expected %q but got %q", expected, repeated)
+	}
+}
+
+func BenchmarkRepeat(b *testing.B) {
+	for b.Loop() {
+		Repeat("a", 7)
+	}
+}
+
+func ExampleRepeat() {
+	repeated := Repeat("d", 10)
+	fmt.Println(repeated)
+	// Output: dddddddddd
+}
