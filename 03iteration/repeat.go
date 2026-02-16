06617164d30445a6af90907be9ef965f72b6ea87
rangga-prangwedana <vergil1st@gmail.com>
2026-02-16 14:09:49 +0700

    fix: Trying repeated strings module

--- 03iteration/repeat.go
+++ 03iteration/repeat.go
@@ -3,10 +3,5 @@ package iteration
 import "strings"
 
 func Repeat(character string, repeatCount int) string {
-	var repeated strings.Builder
-	
-	for i := 0; i < repeatCount; i++ {
-		repeated.WriteString(character)
-	}
-	return repeated.String()
+	return strings.Repeat(character, repeatCount)
 }
