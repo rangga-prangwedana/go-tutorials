f62307f27921e73468309e1f9502cf2c289aebd5
rangga-prangwedana <vergil1st@gmail.com>
2026-03-04 19:23:33 +0700

    fix: Finished struct exercise

--- 05struct/shapes_test.go
+++ 05struct/shapes_test.go
@@ -13,11 +13,23 @@ func TestPerimeter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
-	rectangle := Rectangle{12.0, 6.0}
-	got := Area(rectangle)
-	want := 72.0
 
-	if got != want {
-		t.Errorf("got %.2f want %2.f", got, want)
+	areaTests := []struct {
+		name string
+		shape Shape
+		hasArea float64
+	}{
+		{name: "Rectangle", shape: Rectangle{12.0, 6.0}, hasArea: 72.0},
+		{name: "Circle", shape: Circle{10}, hasArea: 314.1592653589793},
+		{name: "Triangle", shape: Triangle{12, 6}, hasArea: 36.0},
+	}
+
+	for _, tt := range areaTests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := tt.shape.Area()
+			if got != tt.hasArea {
+				t.Errorf("got %g want %g", got, tt.hasArea)
+			}
+		})
 	}
 }
