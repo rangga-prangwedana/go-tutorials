f435a77104e9a15bcc8ec1ccf9144a14652ba467
rangga-prangwedana <vergil1st@gmail.com>
2026-03-26 20:29:28 +0700

    fix: Added new test and refactor

--- 06pointers/wallet_test.go
+++ 06pointers/wallet_test.go
@@ -5,15 +5,37 @@ import (
 )
 
 func TestWallet(t *testing.T) {
-	wallet := Wallet{}
 
-	wallet.Deposit(10)
+	assertBalance := func(t testing.TB, wallet Wallet, want Bitcoin) {
+		t.Helper()
+		got := wallet.Balance()
 
-	got := wallet.Balance()
-
-	want := 10
-
-	if got != want {
-		t.Errorf("got %d want %d", got, want)
+		if got != want {
+			t.Errorf("got %s want %s", got, want)
+		}
 	}
+
+	t.Run("deposit", func(t *testing.T) {
+		wallet := Wallet{}
+		wallet.Deposit(Bitcoin(20))
+		assertBalance(t, wallet, Bitcoin(20))
+	})
+
+	t.Run("withdraw", func(t *testing.T) {
+		wallet := Wallet{balance: Bitcoin(20)}
+		wallet.Withdraw(Bitcoin(10))
+		assertBalance(t, wallet, Bitcoin(10))
+	})
+
+	t.Run("withdraw insufficient funds", func(t *testing.T) {
+		startingBalance := Bitcoin(20)
+		wallet := Wallet{startingBalance}
+		err := wallet.Withdraw(Bitcoin(100))
+
+		assertBalance(t, wallet, startingBalance) 
+
+		if err != nil {
+			t.Errorf("Wanted an error but didn't get one")
+		}
+	})
 }
